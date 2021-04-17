bd8a4b38280e6ca7e104ac03e5a037cc82ff5029
Neelam618 <neelamchavan410@gmail.com>
2021-04-17 19:38:32 +0530

    added menu and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import { Button } from 'antd';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less';
+import AppHeader from './components/common/Header';
+
+import { Layout } from 'antd';
+const { Header } = Layout;
 
 function App() {
   return (
-   
-  );
+      <Layout className="mainLayout">
+        <Header>
+          <AppHeader />
+        </Header>
+      </Layout>
+    );
 }
 
 export default App;
