316eaf2899042125a912339dc93f97b3f41dd17c
Neelam618 <neelamchavan410@gmail.com>
2021-04-17 20:56:53 +0530

    hero component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less';
 import AppHeader from './components/common/Header';
+import AppHome from '../src/views/Home';
 
 import { Layout } from 'antd';
-const { Header } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Header>
           <AppHeader />
         </Header>
+        <Content>
+          <AppHome />
+        </Content>
       </Layout>
     );
 }
---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AppHero from '../components/Home/Hero';
+
+function AppHome(){
+    return (
+        <div className='main'>
+            <AppHero />
+        </div>
+    );
+}
+export default AppHome;
